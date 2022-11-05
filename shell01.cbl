ab25a3649dfddde8a45ace856810b3020807ab1f
E124BG <graak@live.fr>
2022-11-05 18:33:29 +0100

    modified shell01 to include WORKING-STORAGE SECTION

---
+++ shell01.cbl
@@ -0,0 +1,17 @@
+      ******************************************************************
+      * Author: Eliott Bonte
+      * Date: 01/11/2022
+      * Purpose: This is my first shell, I can use it as minimal skeleton of a
+      * COBOL program, to save time.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. COBSHL01.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+
+           WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+       PROGRAM-BEGIN.
+
+       PROGRAM-DONE.
+           STOP RUN.
